12f07652c9e10b52e3a8adcacd09455e9a82d4c6
scarlet <vanneser21@gmail.com>
2025-08-10 14:35:22 -0400

    actualizacion de commit

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,39 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let = amigos =[];
+let amigoInput = document.querySelector("#amigo");
+let listaDeAmigos = document.querySelector("#listaAmigos");
+let amigoSecreto = document.querySelector("#resultado");
+
+const agregarAmigo = () =>{
+    if (amigo.amigoInput ==""){
+        alert("por favor, ingrese un nombre:");
+
+        }else{
+            amigos.push(amigo.amigoInput.value);
+            console.log (amigos);
+            amigoInput.value ="";
+
+            agregarAmigoLista()
+
+            }
+    }
+
+    const agregarAmigoLista = () =>{
+        listaDeAmigos.innerHTML = "";
+        for (let i = 0; i < amigos.lenth; i ++){
+            listaDeAmigos.innerHTML+='<li>${amigo [i]} </li>';
+        }
+    }
+
+    const sortearAmigo =() =>{
+        if (amigos.lenth === 0){
+            alert("debe ingresar un amigo para iniciar el sorteo");
+
+        }else{
+            let numeroAmigo = Math.floor(Math.random() *
+        amigos.lenth);
+
+        amigoSecreto.textContent = amigos[numeroAmigo];
+    } 
+        }
+
