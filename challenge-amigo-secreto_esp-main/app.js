4d13ec489abbeec8264d0aac4a674a3c67b1b0bc
scarlet-crypto <vanneser21@gmail.com>
2025-10-05 16:25:37 -0300

    Actualizar app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let  amigos =[];
 let amigoInput = document.querySelector("#amigo");
 let listaDeAmigos = document.querySelector("#listaAmigos");
